b348a75c4ba45491ded0a46648b0e3f27844f7f2
Karly <karlysoldner@n3-25-158.dhcp.drexel.edu>
2017-02-01 18:03:52 -0500

    added AstroSign but not done yet

--- js/main.js
+++ js/main.js
@@ -12,5 +12,17 @@ function calZodiac(){
 	var outputArea = document.getElementById('displayMsg');
 	outputArea.innerHTML = 'Hello ' + firstName + ' you were born on a ' + WeekDays[whcihDayNdx];
 
+	var whichMonth = birthDayObj.getMonth();
+	console.log('whichMonth is ' + whichMonth);
+
+	var whichDayOfMonth = birthDayObj.getDate();
+	console.log('whichDayOfMonth is ' + whichDayOfMonth);
+
+	whichMonth ++;
+	whichDayOfMonth ++;
+
+	var AstroSign;
+	
+
 	return false;
 }
