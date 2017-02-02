7c62e3284aa5f364b7547fe7283bf1018204085d
Karly <karlysoldner@Karlys-MacBook-Air.local>
2017-02-01 21:55:20 -0500

    fucked up the folders?

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ function calZodiac(){
 
 	var outputArea = document.getElementById('displayMsg');
 	outputArea.innerHTML = 'Hello '+ firstName + ' you were born on a ' + WeekDays[whichDayNdx] + ' your zodiac sign is ' + AstroSign;
+	alert(outputArea);
 	
 
 	return false;
