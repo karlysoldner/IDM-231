2569270814f2d65c503e7632bcfdd7f24694987f
Karly <karlysoldner@Karlys-MacBook-Air.local>
2017-02-01 22:37:56 -0500

    whichMonth

--- main.js
+++ main.js
@@ -26,27 +26,27 @@ function calZodiac(){
 	if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
 		AstroSign = "Cap";
 	}
-	else if ((whichDayOfMonth == 1 && day >= 21) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+	else if ((whichMonth == 1 && whichDayOfMonth >= 21) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
     AstroSign = "Aqu";
-  } else if((whichDayOfMonth == 2 && day >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+  } else if((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
     AstroSign = "Pis";
-  } else if((whichDayOfMonth == 3 && day >= 21) || (whichMonth == 4 && whichDayOfMonth <= 20)) {
+  } else if((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 20)) {
     AstroSign = "Ari";
-  } else if((whichDayOfMonth == 4 && day >= 21) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+  } else if((whichMonth == 4 && whichDayOfMonth >= 21) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
     AstroSign = "Tau";
-  } else if((whichDayOfMonth == 5 && day >= 21) || (whichMonth == 6 && whichDayOfMonth <= 20)) {
+  } else if((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 20)) {
     AstroSign = "Gem";
-  } else if((whichDayOfMonth == 6 && day >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+  } else if((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
     AstroSign = "Can";
-  } else if((whichDayOfMonth == 7 && day >= 23) || (whichMonth == 8 && whichDayOfMonth <= 23)) {
+  } else if((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 23)) {
     AstroSign = "Leo";
-  } else if((whichDayOfMonth == 8 && day >= 24) || (whichMonth == 9 && whichDayOfMonth <= 23)) {
+  } else if((whichMonth == 8 && whichDayOfMonth >= 24) || (whichMonth == 9 && whichDayOfMonth <= 23)) {
     AstroSign = "Vir";
-  } else if((whichDayOfMonth == 9 && day >= 24) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+  } else if((whichMonth == 9 && whichDayOfMonth >= 24) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
     AstroSign = "Lib";
-  } else if((whichDayOfMonth == 10 && day >= 24) || (whichMonth == 11 && whichDayOfMonth <= 22)) {
+  } else if((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 22)) {
     AstroSign = "Sco";
-  } else if((whichDayOfMonth == 11 && day >= 23) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
+  } else if((whichMonth == 11 && whichDayOfMonth >= 23) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
     AstroSign = "Sag";
 }
 	else
