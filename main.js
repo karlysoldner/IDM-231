8a692d5a36bc99efc449b419dfa33dda54178db5
Karly <karlysoldner@Karlys-MacBook-Air.local>
2017-02-01 23:34:01 -0500

    semantics

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ function calZodiac(){
 
 
 	var outputArea = document.getElementById('displayMsg');
-	outputArea.innerHTML = 'Hello '+ firstName + ' ' + lastName + ' you are ' + age + ' years old and' +' you were born on a ' + WeekDays[whichDayNdx] + '.' + ' Your zodiac sign is ' + AstroSign;
+	outputArea.innerHTML = 'Hello '+ firstName + ' ' + lastName + ' you are ' + age + ' years old and' +' you were born on a ' + WeekDays[whichDayNdx] + '.' + ' Your zodiac sign is ' + AstroSign + '.';
 	
 	
 
